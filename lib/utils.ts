b977572a3bfb3f71448f7907939d7903f79c4f2e
Chanawin-kmpn <pond_chanawin@hotmail.com>
2025-01-21 11:23:16 +0700

    create: create editor component

--- lib/utils.ts
+++ lib/utils.ts
@@ -11,12 +11,28 @@ export const throttle = (func: Function, limit: number) => {
 			lastRan = Date.now();
 		} else {
 			clearTimeout(lastFunc);
-			lastFunc = setTimeout(function () {
-				if (Date.now() - lastRan >= limit) {
-					func.apply(null, args);
-					lastRan = Date.now();
-				}
-			}, limit - (Date.now() - lastRan));
+			lastFunc = setTimeout(
+				function () {
+					if (Date.now() - lastRan >= limit) {
+						func.apply(null, args);
+						lastRan = Date.now();
+					}
+				},
+				limit - (Date.now() - lastRan)
+			);
 		}
 	};
 };
+
+export function moveCursorWithinInput(
+	input: HTMLInputElement,
+	position: number
+) {
+	// Super old browsers (like, IE?) don't support this.
+	if (!input?.setSelectionRange) {
+		return;
+	}
+
+	input?.focus();
+	input?.setSelectionRange(position, position);
+}
