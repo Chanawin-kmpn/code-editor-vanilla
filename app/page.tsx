8b1c282c5a1c1ab6e352c4efc64a569549d24630
Chanawin-kmpn <pond_chanawin@hotmail.com>
2025-01-24 14:23:54 +0700

    update: update page layout for each playground section

--- app/page.tsx
+++ app/page.tsx
@@ -31,59 +31,72 @@ export default function Home() {
 }
 	  `.trim();
 	return (
-		<div className="max-w-[90rem] mx-auto">
-			<Playground
-				id="demo-1"
-				title="CodePen Layout Demo"
-				html={htmlCode}
-				css={cssCode}
-				mode="default"
-				layoutMode="codepen"
-				size="normal"
-				centered={true}
-				boxSizing="border-box"
-				splitRatio="0.5"
-				startFullscreened={false}
-			/>
-			<Playground
-				id="demo-2"
-				title="Side by side Layout Demo"
-				html={htmlCode}
-				mode="default"
-				layoutMode="side-by-side"
-				size="normal"
-				centered={true}
-				boxSizing="border-box"
-				splitRatio="0.5"
-				startFullscreened={false}
-			/>
+		<div className="max-w-[90rem] mx-auto my-48">
+			<h1 className="text-4xl text-center mb-8">Playground</h1>
+			<div>
+				<h2 className="text-2xl  mb-4">CodePen Layout Demo</h2>
+				<Playground
+					id="demo-1"
+					title="CodePen Layout Demo"
+					html={htmlCode}
+					css={cssCode}
+					mode="default"
+					layoutMode="codepen"
+					size="normal"
+					centered={true}
+					boxSizing="border-box"
+					splitRatio="0.5"
+					startFullscreened={false}
+				/>
+			</div>
+			<div>
+				<h2 className="text-2xl  mb-4">Side by side Layout Demo</h2>
+				<Playground
+					id="demo-2"
+					title="Side by side Layout Demo"
+					html={htmlCode}
+					mode="default"
+					layoutMode="side-by-side"
+					size="normal"
+					centered={true}
+					boxSizing="border-box"
+					splitRatio="0.5"
+					startFullscreened={false}
+				/>
+			</div>
 
-			<Playground
-				id="demo-3"
-				title="Tabbed Layout Demo"
-				html={htmlCode}
-				css={cssCode}
-				mode="default"
-				layoutMode="tabbed"
-				size="normal"
-				centered={true}
-				boxSizing="border-box"
-				splitRatio="0.5"
-				startFullscreened={false}
-			/>
-			<Playground
-				id="demo-4"
-				title="Vertical stack Layout Demo"
-				html={htmlCode}
-				css={cssCode}
-				mode="default"
-				layoutMode="vertical-stack"
-				size="normal"
-				centered={true}
-				boxSizing="border-box"
-				splitRatio="0.5"
-				startFullscreened={false}
-			/>
+			<div>
+				<h2 className="text-2xl  mb-4">Tabbed Layout Demo</h2>
+				<Playground
+					id="demo-3"
+					title="Tabbed Layout Demo"
+					html={htmlCode}
+					css={cssCode}
+					mode="default"
+					layoutMode="tabbed"
+					size="normal"
+					centered={true}
+					boxSizing="border-box"
+					splitRatio="0.5"
+					startFullscreened={false}
+				/>
+			</div>
+			<div>
+				<h2 className="text-2xl  mb-4">Vertical stack Layout Demo</h2>
+				<Playground
+					id="demo-4"
+					title="Vertical stack Layout Demo"
+					html={htmlCode}
+					css={cssCode}
+					mode="default"
+					layoutMode="vertical-stack"
+					size="normal"
+					centered={true}
+					boxSizing="border-box"
+					splitRatio="0.5"
+					startFullscreened={false}
+				/>
+			</div>
 		</div>
 	);
 }
